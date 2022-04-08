bf59d2ee7bc8e85b774739976a3b40f7a6c148ae
thomas martial <thomas.martial@insa-lyon.fr>
2022-04-08 09:35:12 +0200

    Initial making of the Database

---
+++ BDD.py
@@ -0,0 +1,43 @@
+film_id = {"Encanto" : 1,
+        "Titanic": 2,
+        "Turning red": 3,
+        "Avatar": 4 , 
+        "Wolf of Wall street": 5}
+print("id of the film Encanto :", film_id["Encanto"])
+
+film_type_list = [ "Action",
+                "Animated",
+                "Comedy",
+                "Crime",
+                "Documentary", 
+                "Drama", 
+                "Fantastic", 
+                "Horror", 
+                "Romantic", 
+                "Science Fiction",
+                "Thriller"]
+print("List of type for the films", film_type_list)
+
+film_type = {"Encanto": film_type_list[1],
+        "Titanic": film_type_list[8],
+        "Turning red": film_type_list[1],
+        "Avatar": film_type_list[9],
+        "Wolf of Wall street": film_type_list[2]}
+print("Type of the film Encanto :",film_type["Encanto"])
+
+film_url = {"Encanto": "https://en.wikipedia.org/wiki/Encanto_(film)",
+            "Titanic": "https://en.wikipedia.org/wiki/Titanic_(1997_film)", 
+            "Turning red":"https://en.wikipedia.org/wiki/Turning_Red",
+            "Avatar":"https://en.wikipedia.org/wiki/Avatar_(2009_film)", 
+            "Wolf of Wall street": "https://en.wikipedia.org/wiki/The_Wolf_of_Wall_Street_(2013_film)"}
+print("Url of the film Encanto :",film_url["Encanto"])
+
+film_year = {"Encanto": "2021",
+        "Titanic": "1997",
+        "Turning red":"2022",
+        "Avatar":"2009",
+        "Wolf of Wall street": "2013" }
+print("Release year of the film Encanto :",film_year["Encanto"])
+
+#film_autor = 
+
