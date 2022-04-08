1c19b132fa8aea2fe6abc10c07882c6729881088
Danmeierzozo <danmeierzozo@gmail.com>
2022-04-08 12:33:55 +0200

    add welcome function and movies function

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask import request, make_response, render_template,redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def welcome():
+    app.logger.debug('serving root URL /')
+    return render_template('Welcome.html')
+
+@app.route('/movies')
+def movies():
+    app.logger.debug('serving root URL /')
+    return render_template('movies.html')
