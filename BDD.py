2905b2aaaff49aff77843822500ffc6a1b8f083a
thomas martial <thomas.martial@insa-lyon.fr>
2022-04-08 11:15:09 +0200

    Update of the Database

--- BDD.py
+++ BDD.py
@@ -11,48 +11,40 @@ class Type:
         Science_Fiction = "Science Fiction"
         Thriller = "Thriller"
 
-class Starring:
-        starring_Encanto = ["Stephanie Beatriz", "Maria Cecilia Botero", "John Leguizamo", "Mauro Castillo"]
-        starring_Titanic = ["Leonardo DiCaprio", "Kate Winslet", "Billy Zane", "Kathy Bates", "Frances Fisher"]
-        starring_Turning_Red = ["Rosalie Chiang", "Sandra Oh", "Ava Morse", "Hyein Park", "Orion Lee"]
-        starring_Avatar = ["Sam Worthington", "Zoe Saldana", "Stephen Lang", "Michelle Rodriguez", "Sigourney Weaver" ]
-        starring_Wolf_of_Wall_Street = ["Leonardo DiCaprio", "Jonah Hill", "Margot Robbie", "Matthew McConaughey", "Kyle Chandler"]
-
-
 FILMS = [
         {'name':'Encanto',
         'id' : 1,
         'Type': Type.Animated,
         'url':'https://en.wikipedia.org/wiki/Encanto_(film)',
         'year': '2021',
-        'starring': Starring.starring_Encanto
+        'starring': ["Stephanie Beatriz", "Maria Cecilia Botero", "John Leguizamo", "Mauro Castillo"]
         },
         {'name':'Titanic',
         'id' : 2,
         'Type': Type.Romantic,
         'url':'https://en.wikipedia.org/wiki/Titanic_(1997_film)',
         'year': '1997',
-        'starring': Starring.starring_Titanic
+        'starring': ["Leonardo DiCaprio", "Kate Winslet", "Billy Zane", "Kathy Bates", "Frances Fisher"]
         },
         {'name':'Turning red',
         'id' : 3,
         'Type': Type.Animated,
         'url':'https://en.wikipedia.org/wiki/Turning_Red',
         'year': '2022',
-        'starring': Starring.starring_Turning_Red
+        'starring': ["Rosalie Chiang", "Sandra Oh", "Ava Morse", "Hyein Park", "Orion Lee"]
         },
         {'name':'Avatar',
         'id' : 4,
         'Type': Type.Science_Fiction,
         'url':'https://en.wikipedia.org/wiki/Avatar_(2009_film)',
         'year': '2009',
-        'starring': Starring.starring_Avatar
+        'starring': ["Sam Worthington", "Zoe Saldana", "Stephen Lang", "Michelle Rodriguez", "Sigourney Weaver" ]
         },
         {'name':'Wolf of Wall Street',
         'id' : 5,
         'Type': Type.Comedy,
         'url':'https://en.wikipedia.org/wiki/The_Wolf_of_Wall_Street_(2013_film)',
         'year': '2013',
-        'starring': Starring.starring_Wolf_of_Wall_Street
+        'starring': ["Leonardo DiCaprio", "Jonah Hill", "Margot Robbie", "Matthew McConaughey", "Kyle Chandler"]
         },
         ]
